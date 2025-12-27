b1d076624c2873006c0b2dc31d7eece72251b806
mdtanzirhossain <mdtanzirhossainm@gmail.com>
2025-12-27 13:19:29 +0600

    while and for loop

---
+++ Fundamentals/loop.php
@@ -0,0 +1,19 @@
+<?php
+//  while loop
+//  if condition met, loop will be conducted in the block. 
+//  if condition is not met, loop will not go through the block.
+
+// 
+
+// for loop
+// ascending order
+// for($i = 1; $i <=5; $i++){
+//     echo"$i\n";
+// }
+for($i = 5; $i >=1; $i--){
+    echo"$i\n";
+}
+// for($i = 1; $i <= 5; $i++){
+//     echo "5 x $i  = " . (5 * $i)."\n";
+// }
+
---
+++ Fundamentals/switch.php
@@ -0,0 +1,4 @@
+<?php
+
+// the difference between if else and switch is that if condition is met, switch print and breaks. It does not go forward.
+// in if else, it does not 

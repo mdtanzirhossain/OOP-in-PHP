d8293e6ba2814e8bbbeac01bdfe4da4ebcc4f7fc
mdtanzirhossain <mdtanzirhossainm@gmail.com>
2025-12-28 00:01:45 +0600

    array

---
+++ Fundamentals/array.php
@@ -0,0 +1,17 @@
+<?php
+// array stores multiple data type
+// // $fruit = array(
+//     "Mango", "Jackfruit", "Litchi"
+// );
+// var_dump($fruit);
+// print_r($fruit);
+
+// Indexed Array
+
+$student = [
+    'name' => 'Abdur Rahim', /*This is called key value pair  */
+    'age' => '25',
+    'department' => 'Mathmatics',
+];
+// print_r($student);
+echo $student['age'];
---
+++ Fundamentals/function.php
@@ -0,0 +1,2 @@
+<?php
+//  if I declare a variable inside a block or within second bracket, it becomes local.

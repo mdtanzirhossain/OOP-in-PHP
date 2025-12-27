ae40ddff3327bd2956e48a1ba13460d12a4171b6
mdtanzirhossain <mdtanzirhossainm@gmail.com>
2025-12-27 14:03:20 +0600

    while, for and do while loop

--- Fundamentals/loop.php
+++ Fundamentals/loop.php
@@ -6,14 +6,26 @@
 // 
 
 // for loop
+
 // ascending order
 // for($i = 1; $i <=5; $i++){
 //     echo"$i\n";
 // }
-for($i = 5; $i >=1; $i--){
-    echo"$i\n";
-}
+// for($i = 5; $i >=1; $i--){
+//     echo"$i\n";
+// }
 // for($i = 1; $i <= 5; $i++){
 //     echo "5 x $i  = " . (5 * $i)."\n";
 // }
+/* Difference between while and for loop
+in case of for loop, we know how many iteration will be there.
+if we do not know, use while loop  */
 
+// do while loop
+// 1st iteration happpen, without checking the condition.
+
+$i = 1;
+do{
+    echo "Login Attempt ". $i ."\n";
+    $i++;
+}while($i <=4);

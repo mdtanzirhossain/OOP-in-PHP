ad9e0e2ac966e3f3a774069e5b0a615eaa876eb6
mdtanzirhossain <mdtanzirhossainm@gmail.com>
2025-12-30 08:14:34 +0600

    function

--- Fundamentals/array.php
+++ Fundamentals/array.php
@@ -6,12 +6,24 @@
 // var_dump($fruit);
 // print_r($fruit);
 
-// Indexed Array
 
-$student = [
-    'name' => 'Abdur Rahim', /*This is called key value pair  */
-    'age' => '25',
-    'department' => 'Mathmatics',
-];
-// print_r($student);
-echo $student['age'];
+// There are 3 types of Array:
+// Indexed Array
+// Associative Array
+// Multidimensional Array
+
+// $student = [
+//     'name' => 'Abdur Rahim', /*This is called key value pair  */
+//     'age' => '25',
+//     'department' => 'Mathmatics',
+// ];
+// // print_r($student);
+// echo $student['age'];
+
+$data = ['name' => 'Abdur Rahim', 'age' =>25];
+$serialized = serialize($data);
+// string to json
+$json =  json_encode($data);
+print_r($json);
+// print_r($serialized);
+
--- Fundamentals/function.php
+++ Fundamentals/function.php
@@ -1,2 +1,8 @@
 <?php
 //  if I declare a variable inside a block or within second bracket, it becomes local.
+
+function greeting(){
+    echo "Hello World";
+}
+
+greeting();
